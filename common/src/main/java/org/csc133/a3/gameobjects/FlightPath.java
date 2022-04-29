c7dbaae5fa03d2e90a5b2478c0f885f32795482d
gregmoua19 <gregmoua@gmail.com>
2022-04-29 00:20:46 -0700

    Added most of the new files -Began refactoring helicopter into state pattern -Began creating new zoom command and flight path class

---
+++ common/src/main/java/org/csc133/a3/gameobjects/FlightPath.java
@@ -0,0 +1,29 @@
+package org.csc133.a3.gameobjects;
+
+import com.codename1.ui.Graphics;
+import com.codename1.ui.geom.Point;
+
+public class FlightPath extends GameObject{
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public void draw(Graphics g, Point containerOrigin) {
+
+    }
+
+    class BezierCurve {}
+}
